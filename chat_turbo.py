fc7aed0dd0b573f5f5d68e43be5b000b7e5b3432
joseitohuci <106645596+joseitohuci@users.noreply.github.com>
2023-08-14 08:35:31 -0400

    chat_turbo.py

--- chat_turbo.py
+++ chat_turbo.py
@@ -9,23 +9,23 @@ emoji_user = "🙋"
 #load_dotenv()  # carrega as variáveis de ambiente do arquivo .env
 #openai.api_key = os.getenv('SENHA_OPEN_AI')
 
-openai.api_key = ["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"]
+openai.api_key = ["ClaveOPENAI"]
 
 streamlit.title(f'{emoji_robo} Pergunte ao Jarvis')
 streamlit.write('***')
 
 if 'hst_conversa' not in streamlit.session_state:
     streamlit.session_state.hst_conversa = []
-pergunta = streamlit.text_input('Digite sua pergunta')
+pregunta = streamlit.text_input('Digite sua pregunta: ')
 
 col1, col2 = streamlit.columns(2)
 
 with col1:
-    btn_enviar = streamlit.button("Enviar Pergunta")
+    btn_enviar = streamlit.button("Enviar Pregunta")
 with col2:
-    btn_limpar = streamlit.button("Limpar Conversa")
+    btn_limpar = streamlit.button("Limpar Conversacion")
 if btn_enviar: 
-    streamlit.session_state.hst_conversa.append({"role": "user", "content": pergunta})
+    streamlit.session_state.hst_conversa.append({"role": "user", "content": pregunta})
     retorno_openai = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo", 
         messages = streamlit.session_state.hst_conversa,
@@ -37,14 +37,14 @@ if btn_enviar:
          "content": retorno_openai['choices'][0]['message']['content']})
 if btn_limpar: # boton "Limpar Conversa"
     streamlit.session_state.hst_conversa = [] # redefine a lista de histórico de conversa para vazia
-    pergunta = '' # redefine o valor do input para uma string vazia
+    pregunta = '' # redefine o valor do input para uma string vazia
 if len(streamlit.session_state.hst_conversa) > 0:
     for i in range(len(streamlit.session_state.hst_conversa)):
         if i % 2 == 0:
             with streamlit.container():
-                streamlit.write(f"{emoji_user} Você: " + streamlit.session_state.hst_conversa[i]['content'])
+                streamlit.write(f"{emoji_user} Tu: " + streamlit.session_state.hst_conversa[i]['content'])
         else:
             with streamlit.container():
-                streamlit.write(f"{emoji_robo} Resposta da IA: " + streamlit.session_state.hst_conversa[i]['content'])
+                streamlit.write(f"{emoji_robo} Respuesta de IA: " + streamlit.session_state.hst_conversa[i]['content'])
 #informaciones del autor
-streamlit.sidebar.markdown("<h3 style='text-align: center; font-size: 20px; color: Red'>By Melo Jr &reg - 2023</h3>", unsafe_allow_html=True)
+streamlit.sidebar.markdown("<h3 style='text-align: center; font-size: 20px; color: Red'>By ImJoseitoh &reg - 2023</h3>", unsafe_allow_html=True)
