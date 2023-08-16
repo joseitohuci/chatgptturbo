586dd7619723a0fa8348e8eb5de111e345d186fb
joseitohuci <106645596+joseitohuci@users.noreply.github.com>
2023-08-15 22:56:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ def enviar_mensaje():
     retorno_openai = openai.ChatCompletion.create(
         model = "text-davinci-003", 
         messages = streamlit.session_state.hst_conversa,
-        temperature: 0.7
+        temperature: 0.7,
     )
     streamlit.session_state.hst_conversa.append(
         {"role": "user", 
